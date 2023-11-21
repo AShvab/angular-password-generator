f017559ed8fb7aa95c9ea484a91968739e8a48a4
AShvab <shvabovska@gmail.com>
2023-11-21 23:55:30 +0200

    password shown

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  imports: [
+    CommonModule,
+    FormsModule, 
+  ],
 })
 export class AppComponent {
   length = 0;
